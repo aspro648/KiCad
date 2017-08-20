5c49f4515de9c46792f5813aa740d19da475da97
Ken <648.ken@gmail.com>
2017-08-19 17:18:10 -0700

    Adding CC license to sketches

--- projects/Attiny/Attiny85HeartSMD2/firmware/SMD_challenge/SMD_challenge.ino
+++ projects/Attiny/Attiny85HeartSMD2/firmware/SMD_challenge/SMD_challenge.ino
@@ -1,5 +1,26 @@
 
 /*
+This sketch works with either the SMD Challenge:
+https://hackaday.io/project/25265-an-unfortunate-smd-project
+or the I Can Surface Mount Solder Project:
+https://www.instructables.com/id/I-Can-Surface-Mount-Solder/
+
+
+License
+=======
+[Attribution-ShareAlike 3.0 United States (CC BY-SA 3.0 US)](https://creativecommons.org/licenses/by-sa/3.0/us/)
+
+You are free to:
+
+- Share — copy and redistribute the material in any medium or format
+- Adapt — remix, transform, and build upon the material
+
+Under the following terms:
+
+- Attribution — You must give appropriate credit, provide a link to the license, and indicate if changes were made. You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.
+- ShareAlike — If you remix, transform, or build upon the material, you must distribute your contributions under the same license as the original.
+
+
 Heartbeat code based on Emily Lovell's LilyTiny:
 http://dlnmh9ip6v2uc.cloudfront.net/datasheets/E-Textiles/Lilypad/LilyTiny.ino
 https://users.soe.ucsc.edu/~emme/
