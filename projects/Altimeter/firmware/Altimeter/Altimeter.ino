4fee9464a80fdac1f86aee0279098e05fc24a14e
Ken Olsen <kolsen@sigmadzn.com>
2018-07-25 16:47:33 -0700

    updates

---
+++ projects/Altimeter/firmware/Altimeter/Altimeter.ino
@@ -0,0 +1,316 @@
+/*********************************************************************
+This is an example for our Monochrome OLEDs based on SSD1306 drivers
+
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/category/63_98
+
+This example is for a 128x32 size display using I2C to communicate
+3 pins are required to interface (2 I2C and one reset)
+
+Adafruit invests time and resources providing this open source code, 
+please support Adafruit and open-source hardware by purchasing 
+products from Adafruit!
+
+Written by Limor Fried/Ladyada  for Adafruit Industries.  
+BSD license, check license.txt for more information
+All text above, and the splash screen must be included in any redistribution
+
+Libraries used:
+https://github.com/adafruit/Adafruit_SSD1306
+https://github.com/adafruit/Adafruit_MPL3115A2_Library
+https://github.com/adafruit/Adafruit-GFX-Library
+
+Install Breadboard Arduino support from https://www.arduino.cc/en/Tutorial/ArduinoToBreadboard
+(Use Minimal Circuit example to use 8 MHz Internal Crystal).
+
+Modify boards.txt to remove brown-out before flashing bootloader to set fuses:
+atmega328bb.bootloader.low_fuses=0xE2
+atmega328bb.bootloader.high_fuses=0xDA
+atmega328bb.bootloader.extended_fuses=0x07
+
+*********************************************************************/
+
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_MPL3115A2.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+#define NUMFLAKES 10
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+
+
+#define LOGO16_GLCD_HEIGHT 16 
+#define LOGO16_GLCD_WIDTH  16 
+static const unsigned char PROGMEM logo16_glcd_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000 };
+
+#if (SSD1306_LCDHEIGHT != 32)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+Adafruit_MPL3115A2 baro = Adafruit_MPL3115A2();
+
+void setup()   {                
+  Serial.begin(9600);
+
+  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
+  // init done
+
+  // Clear the buffer.
+  display.clearDisplay();
+
+
+
+  // text display tests
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+}
+
+
+void loop() {
+  display.clearDisplay();
+  display.setCursor(0,0);
+  if (! baro.begin()) {
+    Serial.println("Couldnt find sensor");
+    display.println("No");
+    display.println("Sensor?");
+    display.display();
+    return;
+  }
+  float pascals = baro.getPressure();  
+  Serial.print(pascals/3377); Serial.println(" Inches (Hg)");
+  display.print(pascals/3377); display.println(" InHg");
+
+  float tempC = baro.getTemperature();
+  Serial.print(tempC); Serial.println("*C");
+  display.print(tempC, 1); display.println(" C");
+  display.display();
+  delay(1000);
+
+
+}
+
+
+void testdrawbitmap(const uint8_t *bitmap, uint8_t w, uint8_t h) {
+  uint8_t icons[NUMFLAKES][3];
+ 
+  // initialize
+  for (uint8_t f=0; f< NUMFLAKES; f++) {
+    icons[f][XPOS] = random(display.width());
+    icons[f][YPOS] = 0;
+    icons[f][DELTAY] = random(5) + 1;
+    
+    Serial.print("x: ");
+    Serial.print(icons[f][XPOS], DEC);
+    Serial.print(" y: ");
+    Serial.print(icons[f][YPOS], DEC);
+    Serial.print(" dy: ");
+    Serial.println(icons[f][DELTAY], DEC);
+  }
+
+  while (1) {
+    // draw each icon
+    for (uint8_t f=0; f< NUMFLAKES; f++) {
+      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, WHITE);
+    }
+    display.display();
+    delay(200);
+    
+    // then erase it + move it
+    for (uint8_t f=0; f< NUMFLAKES; f++) {
+      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, BLACK);
+      // move it
+      icons[f][YPOS] += icons[f][DELTAY];
+      // if its gone, reinit
+      if (icons[f][YPOS] > display.height()) {
+        icons[f][XPOS] = random(display.width());
+        icons[f][YPOS] = 0;
+        icons[f][DELTAY] = random(5) + 1;
+      }
+    }
+   }
+}
+
+
+void testdrawchar(void) {
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+
+  for (uint8_t i=0; i < 168; i++) {
+    if (i == '\n') continue;
+    display.write(i);
+    if ((i > 0) && (i % 21 == 0))
+      display.println();
+  }    
+  display.display();
+  delay(1);
+}
+
+void testdrawcircle(void) {
+  for (int16_t i=0; i<display.height(); i+=2) {
+    display.drawCircle(display.width()/2, display.height()/2, i, WHITE);
+    display.display();
+    delay(1);
+  }
+}
+
+void testfillrect(void) {
+  uint8_t color = 1;
+  for (int16_t i=0; i<display.height()/2; i+=3) {
+    // alternate colors
+    display.fillRect(i, i, display.width()-i*2, display.height()-i*2, color%2);
+    display.display();
+    delay(1);
+    color++;
+  }
+}
+
+void testdrawtriangle(void) {
+  for (int16_t i=0; i<min(display.width(),display.height())/2; i+=5) {
+    display.drawTriangle(display.width()/2, display.height()/2-i,
+                     display.width()/2-i, display.height()/2+i,
+                     display.width()/2+i, display.height()/2+i, WHITE);
+    display.display();
+    delay(1);
+  }
+}
+
+void testfilltriangle(void) {
+  uint8_t color = WHITE;
+  for (int16_t i=min(display.width(),display.height())/2; i>0; i-=5) {
+    display.fillTriangle(display.width()/2, display.height()/2-i,
+                     display.width()/2-i, display.height()/2+i,
+                     display.width()/2+i, display.height()/2+i, WHITE);
+    if (color == WHITE) color = BLACK;
+    else color = WHITE;
+    display.display();
+    delay(1);
+  }
+}
+
+void testdrawroundrect(void) {
+  for (int16_t i=0; i<display.height()/2-2; i+=2) {
+    display.drawRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4, WHITE);
+    display.display();
+    delay(1);
+  }
+}
+
+void testfillroundrect(void) {
+  uint8_t color = WHITE;
+  for (int16_t i=0; i<display.height()/2-2; i+=2) {
+    display.fillRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4, color);
+    if (color == WHITE) color = BLACK;
+    else color = WHITE;
+    display.display();
+    delay(1);
+  }
+}
+   
+void testdrawrect(void) {
+  for (int16_t i=0; i<display.height()/2; i+=2) {
+    display.drawRect(i, i, display.width()-2*i, display.height()-2*i, WHITE);
+    display.display();
+    delay(1);
+  }
+}
+
+void testdrawline() {  
+  for (int16_t i=0; i<display.width(); i+=4) {
+    display.drawLine(0, 0, i, display.height()-1, WHITE);
+    display.display();
+    delay(1);
+  }
+  for (int16_t i=0; i<display.height(); i+=4) {
+    display.drawLine(0, 0, display.width()-1, i, WHITE);
+    display.display();
+    delay(1);
+  }
+  delay(250);
+  
+  display.clearDisplay();
+  for (int16_t i=0; i<display.width(); i+=4) {
+    display.drawLine(0, display.height()-1, i, 0, WHITE);
+    display.display();
+    delay(1);
+  }
+  for (int16_t i=display.height()-1; i>=0; i-=4) {
+    display.drawLine(0, display.height()-1, display.width()-1, i, WHITE);
+    display.display();
+    delay(1);
+  }
+  delay(250);
+  
+  display.clearDisplay();
+  for (int16_t i=display.width()-1; i>=0; i-=4) {
+    display.drawLine(display.width()-1, display.height()-1, i, 0, WHITE);
+    display.display();
+    delay(1);
+  }
+  for (int16_t i=display.height()-1; i>=0; i-=4) {
+    display.drawLine(display.width()-1, display.height()-1, 0, i, WHITE);
+    display.display();
+    delay(1);
+  }
+  delay(250);
+
+  display.clearDisplay();
+  for (int16_t i=0; i<display.height(); i+=4) {
+    display.drawLine(display.width()-1, 0, 0, i, WHITE);
+    display.display();
+    delay(1);
+  }
+  for (int16_t i=0; i<display.width(); i+=4) {
+    display.drawLine(display.width()-1, 0, i, display.height()-1, WHITE); 
+    display.display();
+    delay(1);
+  }
+  delay(250);
+}
+
+void testscrolltext(void) {
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(10,0);
+  display.clearDisplay();
+  display.println("scroll");
+  display.display();
+  delay(1);
+ 
+  display.startscrollright(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+  delay(1000);
+  display.startscrollleft(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+  delay(1000);    
+  display.startscrolldiagright(0x00, 0x07);
+  delay(2000);
+  display.startscrolldiagleft(0x00, 0x07);
+  delay(2000);
+  display.stopscroll();
+}
--- projects/Altimeter2/README.md
+++ projects/Altimeter2/README.md
@@ -1,48 +1,34 @@
 Altimeter V0
 ------------
 
-![progject](images/Altimeter.JPG) 
+![progject](images/project.png) 
 
-Absolute pressure measurement by NXP MPL3115A2
-
-- Operating range: 20 kPa to 110 kPa absolute pressure
-- Calibrated range: 50 kPa to 110 kPa absolute pressure
-- Calibrated temperature output: −40 °C to 85 °C
-- 20-bit ADC, I2C output
 
 
 Bill Of Materials
 -----------------
 
 - 1 ea., Perfect Purple PCB from OSH Park
-- 1 ea., I2C OLED Display Module 0.91 Inch I2C SSD1306, http://a.co/4WFRMoG
 - 1 ea., SW1 SWITCH REED SPST-NO 500MA 140V, Coto CT10-1530-G1, https://www.digikey.com/short/j58cd0
 - 1 ea., SW2 SWITCH SLIDE SPDT, C&K JS102011SAQN, https://www.digikey.com/short/j58qp5
 - 1 ea., U1 ATMEGA328P-AU-ND 32TQFP, https://www.digikey.com/short/jbpvp7
 - 1 ea., U2 IC ALTIMETER I2C VERSION 8LGA, NXP MPL3115A2R1, https://www.digikey.com/short/j587bt
 - 3 ea., C1, 3, 4, CAP CER 0.1UF 50V X7R 0805, https://www.digikey.com/short/j58qjp
-- 2 ea., C2, 6 CAP CER 10UF 10V X5R 1206, https://www.digikey.com/short/j58q80
+- 1 ea., C2 CAP CER 10UF 10V X5R 1206, https://www.digikey.com/short/j58q80
 - 1 ea., D1 LED ORANGE CLEAR 0805 SMD, https://www.digikey.com/short/j58qzz
 - 1 ea., R1 RES 330 OHM 5% 1/8W 0805, https://www.digikey.com/short/j58qzr
 - 2 ea., R2, R3 RES 1K OHM 5% 1/4W 0805, https://www.digikey.com/short/j58q4m
 - 1 ea., HOLDER BATTERY 20MM COIN, Lynx BAT-HLD-001, https://www.digikey.com/short/j58q42
-- 1 ea., CR2032 Battery, Panasonic CR2032, Digikey P189-ND (requires ground shipping, or buy locally).
-
-
-Prototyping
------------
-
-- Adafruit AMPL3115A2 BAROMETER SENSE BOARD, https://www.digikey.com/short/j5101d
-- Adafruit DISPL OLED GRAPH MONO 128X32 I2C, https://www.digikey.com/short/j51vc0
+- 1 ea. CR2032 Battery, Panasonic CR2032, Digikey P189-ND (requires ground shipping, or buy locally).
 
 Design Files
 ------------
-The electronics were designed using Open Source [KiCad](http://kicad-pcb.org/). Design files are located in the [design_files](design_files/) folder.  You can oogle the [schematic](docs/schematic.pdf).
+The electronics were designed using Open Source [KiCad](http://kicad-pcb.org/). Design files are located in the [design_files](design_files/) folder.  You can oogle the [schematic](docs/Schematic.pdf).
+
 
 Firmware
 --------
-This project is programed using the Open Source [Arduino](https://www.arduino.cc/). I use my Open Source [AVR Programming Shield](https://www.tindie.com/products/MakersBox/yet-another-programming-shield/). The firmware is located in the [firmware](firmware/) folder.
-Note: The pressure sensor is a 3V device (3.6v max).  Use a level translator with the programmer (https://www.sparkfun.com/products/11771) to avoid applying 5V to the sensor during programming!
+TODO
 
 Assembly Instructions
 ---------------------
