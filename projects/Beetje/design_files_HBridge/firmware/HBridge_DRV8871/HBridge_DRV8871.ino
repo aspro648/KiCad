b26edfe1caccdea910a5edc9d48d46bd736dd0c3
Ken <648.ken@gmail.com>
2019-05-17 11:50:56 -0700

    updates

--- projects/Beetje/design_files_HBridge/firmware/HBridge_DRV8871/HBridge_DRV8871.ino
+++ projects/Beetje/design_files_HBridge/firmware/HBridge_DRV8871/HBridge_DRV8871.ino
@@ -4,26 +4,23 @@
  
 
 
-int led = 13;
-int Ain1 = 14; // MISO
-int Ain2 = 16;  // MOSI
-int Bin1 = 10;
-int Bin2 = 9;
+int led = 6;
+int in1 = 10;
+int in2 = 9;
 
 
-int button = 4;
+int button = A2;
 int state = 0;  // 0 = off, 1 = forward, 2 =backward
-
+int speed = 255;
 
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
   pinMode(led, OUTPUT);
-  pinMode(Ain1, OUTPUT);
-  pinMode(Ain2, OUTPUT); 
-  pinMode(Bin1, OUTPUT);
-  pinMode(Bin2, OUTPUT); 
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT); 
   pinMode(button, INPUT_PULLUP);    
+  Serial.begin(9600);
 }
 
 
@@ -35,32 +32,28 @@ void loop() {
     while(!digitalRead(button)){
       delay(10);
     }
+    
     digitalWrite(led, LOW);
     state += 1;
     if (state > 2){
       state = 0;
     }
+    Serial.println(state);
   }
  switch(state){
   case 0:
-    digitalWrite(Ain1, LOW);
-    digitalWrite(Ain2, LOW);
-    digitalWrite(Bin1, LOW);
-    digitalWrite(Bin2, LOW);
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
     digitalWrite(led, LOW);
     break;
   case 1:
-    digitalWrite(Ain1, HIGH);
-    digitalWrite(Ain2, LOW);
-    digitalWrite(Bin1, HIGH);
-    digitalWrite(Bin2, LOW);
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
     digitalWrite(led, HIGH);
     break;  
   case 2:
-    digitalWrite(Ain1, LOW);
-    digitalWrite(Ain2, HIGH);
-    digitalWrite(Bin1, HIGH);
-    digitalWrite(Bin2, LOW);
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
     digitalWrite(led, HIGH);
     break;  
   }
